5dda74f21985950c6b22b25d68f5021e8281a8cc
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-04-25 17:41:50 +0530

    row data

---
+++ data.js
@@ -0,0 +1,57 @@
+export const SERVICES = [
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1737357611/xbcd0xpxjjwhu8snltsl.jpg',
+        heading: 'Asset Management',
+        description: 'Highway asset management ensures the strategic maintenance, upgrade, and operation of roads, bridges, and related infrastructure in the most cost-effective way.',
+        url: '/services/asset-management',
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1736504752/lnhjksh22h7rd7o61nlu.jpg',
+        heading: 'Project Management Consultancy',
+        description: 'Project Management Consultancy oversees construction activities, focusing on scheduling, budgeting, risk management, and control.',
+        url: '/services/project-management-consultancy',
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1732602833/traffic-survey_ovkzmw.png',
+        heading: 'Traffic Engineering',
+        description: 'Traffic surveys collect data on vehicle flow, speed, and patterns to inform roadway design, traffic management, and safety improvements.',
+        url: '/services/traffic-engineering',
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1732529824/NSV_nkf4y8.png',
+        heading: 'Network survey vehicle (NSV)',
+        description: 'It provides real-time data on road conditions and capture roadside amenities, helping with efficient maintenance and planning.',
+        url: '/services/network-survey-vehicle',
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1738756777/swh1dexhdbkidxtqpzzs.png',
+        heading: 'Falling Weight Deflectometer',
+        description: 'FWD measures pavement deflection under load to assess structural strength and guide maintenance or rehabilitation strategies.',
+        url: '/services/falling-weight-deflectometer',
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1737801215/syvgyp9owuvixiwpobzj.jpg',
+        heading: 'BBD Survey',
+        description: 'BBD Survey measures pavement deflection to assess structural strength for maintenance planning.',
+        url: '/services/bbd-survey',
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1732596151/retro-reflectivity_y6y29p.png',
+        heading: 'Retro Reflectivity Survey',
+        description: 'RR Survey measures the reflectiveness of road markings and signs bords, ensuring they meet visibility standards for night time driving safety.',
+        url: '/services/retro-reflectivity-survey',
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1732600506/gpr-sommet_uv8zsx.png',
+        heading: 'Ground Penetrating Radar (GPR)',
+        description: 'GPR detects subsurface conditions and pavement layers non-destructively, helping with roadway assessment and maintenance planning.',
+        url: 'services/ground-penetrating-radar',
+    },
+
+    {
+        imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1732603218/lab-sommet_ex6vtg.png',
+        heading: 'Material Testing Laboratory',
+        description: 'Conducting quality tests on road materials such as soil, aggregate, bitumen, emulsion, and concrete to meet highway project standards.',
+        url: '/services/material-testing-laboratory',
+    },
+];
